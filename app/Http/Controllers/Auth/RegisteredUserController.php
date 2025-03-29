e30913ec20c418a5a52453afd60f62ca5be706d0
Carbone <dfu@myges.fr>
2025-03-29 20:00:11 +0100

    Retrait de l'envoi d'email car non fonctionnel

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,6 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        $user->sendEmailVerificationNotification();
 
         event(new Registered($user));
 
