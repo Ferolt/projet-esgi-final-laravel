ccad86f8dbb0ffee71ae5e39129c0db870c7142d
Carbone <dfu@myges.fr>
2025-03-29 20:00:54 +0100

    Refactor User model to remove MustVerifyEmail implementation and adjust task relationship formatting

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -53,7 +53,7 @@ class User extends Authenticatable implements MustVerifyEmail
     public function tasks(): BelongsToMany
     {
         return $this->belongsToMany(Task::class, 'task_user')
-                    ->using(TaskUser::class)
-                    ->withTimestamps();
+            ->using(TaskUser::class)
+            ->withTimestamps();
     }
 }
